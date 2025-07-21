6075dfbe4e85f6e3e6dee4897f682f594591449b
Carolina Calderon <carolina.calderon@bayteq.com>
2025-07-21 14:58:05 -0500

    web enable project ok

--- composeApp/src/wasmJsMain/kotlin/com/example/kmppractice/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/example/kmppractice/main.kt
@@ -2,10 +2,16 @@ package com.example.kmppractice
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.example.kmppractice.core.di.AppModule
 import kotlinx.browser.document
+import org.koin.core.context.GlobalContext.startKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+
+    startKoin {
+        modules(AppModule())
+    }
     ComposeViewport(document.body!!) {
         App()
     }
